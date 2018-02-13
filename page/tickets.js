b9b1478b5951f706513afcb69f04fdeb339dac86
Németh Bálint <balint.nem3th@gmail.com>
2018-02-13 10:42:45 +0100

    add tickets.html w/ table-datas

---
+++ page/tickets.js
@@ -0,0 +1,55 @@
+'use strict';// eslint-disable-line
+
+/* eslint linebreak-style: ['error', 'windows'] */
+
+function connect(method, query, callback, send) {
+  const xhr = new XMLHttpRequest();
+  // const url = `http://localhost:8080${query}`;
+  const url = `http://localhost:8080/${query}`;
+  // console.log('MyURL', url);
+  xhr.open(method, url);
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      callback(data);
+    }
+  };
+  xhr.send(JSON.stringify(send));
+}
+
+window.addEventListener('load', () => {
+  connect('GET', 'tickets', renderTable, null);
+});
+
+function renderTable(data) {
+  const tableBody = document.getElementById('table-body');
+  tableBody.innerHTML= '';
+  data.rows.forEach((element) => {
+    const row = document.createElement('tr');
+    const id = document.createElement('td');
+    id.textContent = element.id;
+    row.appendChild(id);
+    const reporter = document.createElement('td');
+    reporter.textContent = element.reporter;
+    row.appendChild(reporter);
+    const manufacturer = document.createElement('td');
+    manufacturer.textContent = element.manufacturer;
+    row.appendChild(manufacturer);
+    const serialNumber = document.createElement('td');
+    serialNumber.textContent = element.serialNumber;
+    row.appendChild(serialNumber);
+    const description = document.createElement('td');
+    description.textContent = element.description;
+    row.appendChild(description);
+    const reportedAt = document.createElement('td');
+    reportedAt.textContent = element.reported_at;
+    row.appendChild(reportedAt);
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'DELETE';
+    row.appendChild(deleteButton);
+    tableBody.appendChild(row);
+  });
+}
+
+console.log(':)');
